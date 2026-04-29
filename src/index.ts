90838964b580c4696b5e27db504f4f8d21e7f89c
Vishal Minz <vishalm@mindfiresolutions.com>
2026-04-29 11:32:32 +0530

    setup: express initialized

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const httpPort = process.env.HTTP_PORT ?? '3000';
+const server = http.createServer(app);
+server.listen(httpPort, () => {
+    console.log(`HTTP server listening on port ${httpPort}`);
+})
